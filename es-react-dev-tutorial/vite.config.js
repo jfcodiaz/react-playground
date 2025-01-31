59c2fabeb05d79ebc6cf50fdc951620201cce776
FcoDiaz <jfcodiaz@gmail.com>
2025-01-31 16:00:13 -0600

    ⚙️ Configure vite to expose hmr to hosts browser ✏️

--- es-react-dev-tutorial/vite.config.js
+++ es-react-dev-tutorial/vite.config.js
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
-    host: true
+    host: true,  // Permite que el contenedor acepte conexiones externas
+    port: 3000,       // Cambia el puerto al 3000
+    strictPort: true, // Asegura que no cambie automáticamente
+    watch: {
+      usePolling: true, // Forzar polling para detectar cambios en Docker
+    },
+    hmr: {
+      clientPort: 3000, // Especifica el puerto correcto para HMR
+      protocol: 'ws',   // Usa WebSockets en lugar de HTTP
+    }
   },
   plugins: [react()],
 })
